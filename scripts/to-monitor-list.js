28506f7e5d51538be31b11fc16c735b0a7b9d951
Eejit <76887639+Eejit43@users.noreply.github.com>
2023-04-27 08:52:18 -0400

    improve searches

--- scripts/to-monitor-list.js
+++ scripts/to-monitor-list.js
@@ -4,30 +4,29 @@ const toCheck = {
     categories: [
         { id: 'empty-short-description', category: 'Pages with empty short description' }, //
         { id: 'disallowed-displaytitle', category: 'Pages with disallowed DISPLAYTITLE modifications' },
-        { id: 'defaultsort-conflicts', category: 'Pages with DEFAULTSORT conflicts' },
-        { id: 'incorrect-protection-templates', category: 'Wikipedia pages with incorrect protection templates', notNamespace: 'Article' }
+        { id: 'defaultsort-conflicts', category: 'Pages with DEFAULTSORT conflicts' }
     ],
     searches: [
         { id: 'wp-list-article', search: 'insource:"Wikipedia list article"' },
         { id: 'wm-list-article', search: 'insource:"Wikimedia list article"' },
         { id: 'template-incorrect-namespace', search: 'incategory:"Pages with templates in the wrong namespace" -intitle:"Contactpage-arbcom-block-appeal-prior-onwiki" -subpageof:"SDZeroBot"' },
-        { id: 'draft-displaytitles', search: 'insource:/\\{\\{DISPLAYTITLE:Draft:/i' },
-        { id: 'draft-defaultsorts', search: 'insource:/\\{\\{DEFAULTSORT:Draft:/i' },
-        { id: 'mainspace-category-links', search: "insource:/((\\]{2}|\\}{2})[^ -�,]|[^ -�,]{2,})\\[\\[:[Cc]ategory:[A-Za-z0-9_\\-–' ]*\\|? ?\\]\\]/" },
-        { id: 'external-link-header', search: 'insource:/= ?External link ?=/i' },
-        { id: 'reference-header', search: 'insource:/= ?Reference ?=/i' },
-        { id: 'pagename-article-namespace', search: 'insource:/\\{\\{PAGENAME\\}\\}/ -insource:"{{PAGENAME}} by default" -insource:"defaults to {{PAGENAME}}" -insource:"Overrides {{PAGENAME}}"' },
+        { id: 'draft-displaytitles', search: 'insource:"Draft" insource:/\\{\\{DISPLAYTITLE:Draft:/i' },
+        { id: 'draft-defaultsorts', search: 'insource:"Draft" insource:/\\{\\{DEFAULTSORT:Draft:/i' },
+        { id: 'mainspace-category-links', search: 'insource:"Category" insource:/((\\]{2}|\\}{2})[^ -�,]|[^ -�,]{2,})\\[\\[:[Cc]ategory:[A-Za-z0-9_\\-–\' ]*\\|? ?\\]\\]/' },
+        { id: 'external-link-header', search: 'insource:"External link" insource:/= ?External link ?=/i' },
+        { id: 'reference-header-1', search: '"Reference" insource:"Reference" insource:/=Reference=/' },
+        { id: 'reference-header-2', search: '"Reference" insource:"Reference" insource:/= Reference =/' },
         { id: 'capitalized-section-titles', search: 'insource:/= ?(Personal Life|Early Life|See Also|External Links) ?=/' },
-        { id: 'bold-text', search: 'insource:/\'{3}Bold text\'{3}/ -indicates -denotes -"Class pole winner"' }, //
-        { id: 'italic-text', search: "insource:/'{2}Italic text'{2}/ -indicates -denotes" },
+        { id: 'bold-text', search: 'insource:"Bold text" insource:/\'{3}Bold text\'{3}/ -indicates -denotes -"Class pole winner"' }, //
+        { id: 'italic-text', search: 'insource:"Italic text" insource:/\'{2}Italic text\'{2}/ -indicates -denotes' },
         { id: 'heading-text', search: 'insource:"Heading text" -intitle:/Markdown|Table of contents|Org-mode|Lightweight markup language|Psalm 60|Spectrum (band)|Warnier/Orr diagram|ImpressPages|Korinna/' },
         { id: 'bulleted-list-item', search: 'insource:"Bulleted list item" -intitle:/Bullet \\(typography\\)|List of World Heritage Sites in the Philippines|Makassan contact with Australia|Presidential Succession Act|Setext/' },
         { id: 'numbered-list-item', search: 'insource:"Numbered list item"' },
         { id: 'nowiki-tag', search: 'insource:"Insert non-formatted text here"' },
-        { id: 'big-text', search: 'insource:/\\<big\\>Big text\\<\\/big\\>/' },
-        { id: 'small-text', search: 'insource:/\\<small\\>Small text\\<\\/small\\>/ -denotes' },
-        { id: 'superscript-text', search: 'insource:/\\<sup\\>Superscript text\\<\\/sup\\>/' },
-        { id: 'subscript-text', search: 'insource:/\\<sub\\>Subscript text\\<\\/sub\\>/' }
+        { id: 'big-text', search: 'insource:"Big text" insource:/\\<big\\>Big text\\<\\/big\\>/' },
+        { id: 'small-text', search: 'insource:"Small text" insource:/\\<small\\>Small text\\<\\/small\\>/ -denotes' },
+        { id: 'superscript-text', search: 'insource:"Superscript text" insource:/\\<sup\\>Superscript text\\<\\/sup\\>/' },
+        { id: 'subscript-text', search: 'insource: "Subscript text" insource:/\\<sub\\>Subscript text\\<\\/sub\\>/' }
     ],
     whatLinksHere: [
         { id: 'default-redirect', title: 'Target page name' }, //

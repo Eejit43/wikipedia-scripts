726d06c2883fb69091d9cdc187b8f6c830737bfd
Eejit <76887639+Eejit43@users.noreply.github.com>
2023-05-05 11:15:33 -0400

    add `afc-submission-mainspace` to `to-monitor-list`

--- scripts/to-monitor-list.js
+++ scripts/to-monitor-list.js
@@ -10,6 +10,7 @@ const toCheck = {
         { id: 'wp-list-article', search: 'insource:"Wikipedia list article"' },
         { id: 'wm-list-article', search: 'insource:"Wikimedia list article"' },
         { id: 'moved-to-draft-mainspace', search: 'hastemplate:"Drafts moved from mainspace"' },
+        { id: 'afc-submission-mainspace', search: 'insource:/\\{\\{AfC submission/' },
         { id: 'template-incorrect-namespace', search: 'incategory:"Pages with templates in the wrong namespace" -intitle:"Contactpage-arbcom-block-appeal-prior-onwiki" -subpageof:"SDZeroBot"', notNamespace: 'Category' },
         { id: 'draft-displaytitles', search: 'insource:"Draft" insource:/\\{\\{DISPLAYTITLE:Draft:/i' },
         { id: 'draft-defaultsorts', search: 'insource:"Draft" insource:/\\{\\{DEFAULTSORT:Draft:/i' },

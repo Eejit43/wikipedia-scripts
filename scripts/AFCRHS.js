9724af5ec7bf1223519e4c995f14cc5a5afef09d
Eejit <76887639+Eejit43@users.noreply.github.com>
2023-05-05 21:20:21 -0400

    `AFCRHS`: rm comment from page check

--- scripts/AFCRHS.js
+++ scripts/AFCRHS.js
@@ -1,7 +1,7 @@
 /* global mw, $ */
 
 (function () {
-    // if (mw.config.get('wgPageName') !== 'Wikipedia:Articles_for_creation/Redirects_and_categories') return;
+    if (mw.config.get('wgPageName') !== 'Wikipedia:Articles_for_creation/Redirects_and_categories') return;
 
     const redirectPageName = mw.config.get('wgPageName').replace(/_/g, ' ');
     const redirectSubmissions = [];

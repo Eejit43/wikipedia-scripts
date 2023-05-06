5f417cd8f85b1dab367996442db89d695067020b
Eejit <76887639+Eejit43@users.noreply.github.com>
2023-05-06 14:06:59 -0400

    `to-monitor-list`: fetch data from json

--- scripts/to-monitor-list.js
+++ scripts/to-monitor-list.js
@@ -1,51 +1,26 @@
 /* global mw */
 
-const toCheck = {
-    categories: [
-        { id: 'empty-short-description', category: 'Pages with empty short description' }, //
-        { id: 'disallowed-displaytitle', category: 'Pages with disallowed DISPLAYTITLE modifications' },
-        { id: 'defaultsort-conflicts', category: 'Pages with DEFAULTSORT conflicts' }
-    ],
-    searches: [
-        { id: 'wp-list-article', search: 'insource:"Wikipedia list article"' },
-        { id: 'wm-list-article', search: 'insource:"Wikimedia list article"' },
-        { id: 'moved-to-draft-mainspace', search: 'hastemplate:"Drafts moved from mainspace"' },
-        { id: 'afc-submission-mainspace', search: 'insource:/\\{\\{AfC submission/' },
-        { id: 'template-incorrect-namespace', search: 'incategory:"Pages with templates in the wrong namespace" -intitle:"Contactpage-arbcom-block-appeal-prior-onwiki" -subpageof:"SDZeroBot"', notNamespace: 'Category' },
-        { id: 'draft-displaytitles', search: 'insource:"Draft" insource:/\\{\\{DISPLAYTITLE:Draft:/i' },
-        { id: 'draft-defaultsorts', search: 'insource:"Draft" insource:/\\{\\{DEFAULTSORT:Draft:/i' },
-        { id: 'mainspace-category-links', search: 'insource:"Category" insource:/((\\]{2}|\\}{2})[^ -�,]|[^ -�,]{2,})\\[\\[:[Cc]ategory:[A-Za-z0-9_\\-–\' ]*\\|? ?\\]\\]/' },
-        { id: 'external-link-header', search: 'insource:"External link" insource:/= ?External link ?=/i' },
-        { id: 'reference-header-1', search: '"Reference" insource:"Reference" insource:/=Reference=/' },
-        { id: 'reference-header-2', search: '"Reference" insource:"Reference" insource:/= Reference =/' },
-        { id: 'capitalized-section-titles', search: 'insource:/= ?(Personal Life|Early Life|See Also|External Links) ?=/' },
-        { id: 'bold-text', search: 'insource:"Bold text" insource:/\'{3}Bold text\'{3}/ -indicates -denotes -"Class pole winner"' }, //
-        { id: 'italic-text', search: 'insource:"Italic text" insource:/\'{2}Italic text\'{2}/ -indicates -denotes' },
-        { id: 'heading-text', search: 'insource:"Heading text" -intitle:/Markdown|Table of contents|Org-mode|Lightweight markup language|Psalm 60|Spectrum (band)|Warnier/Orr diagram|ImpressPages|Korinna/' },
-        { id: 'bulleted-list-item', search: 'insource:"Bulleted list item" -intitle:/Bullet \\(typography\\)|List of World Heritage Sites in the Philippines|Makassan contact with Australia|Presidential Succession Act|Setext/' },
-        { id: 'numbered-list-item', search: 'insource:"Numbered list item"' },
-        { id: 'nowiki-tag', search: 'insource:"Insert non-formatted text here"' },
-        { id: 'big-text', search: 'insource:"Big text" insource:/\\<big\\>Big text\\<\\/big\\>/' },
-        { id: 'small-text', search: 'insource:"Small text" insource:/\\<small\\>Small text\\<\\/small\\>/ -denotes' },
-        { id: 'superscript-text', search: 'insource:"Superscript text" insource:/\\<sup\\>Superscript text\\<\\/sup\\>/' },
-        { id: 'subscript-text', search: 'insource: "Subscript text" insource:/\\<sub\\>Subscript text\\<\\/sub\\>/' }
-    ],
-    whatLinksHere: [
-        { id: 'default-redirect', title: 'Target page name' }, //
-        { id: 'default-image', title: 'File:Example.jpg' }
-    ],
-    transclusions: [
-        { id: 'cat-main-article-namespace', title: 'Template:Cat main' } //
-    ]
-};
-
 mw.loader.using(['mediawiki.util'], () => {
     if (mw.config.get('wgPageName') !== 'User:Eejit43') return;
 
     mw.util.addPortletLink('p-cactions', '#', 'Add counts to monitoring list', 'add-monitoring-counts');
 
-    document.getElementById('add-monitoring-counts').addEventListener('click', (event) => {
+    document.getElementById('add-monitoring-counts').addEventListener('click', async (event) => {
         event.preventDefault();
+
+        const toCheck = JSON.parse(
+            (
+                await new mw.Api().get({
+                    action: 'query',
+                    format: 'json',
+                    prop: 'revisions',
+                    formatversion: 2,
+                    titles: 'User:Eejit43/scripts/to-monitor-list.json',
+                    rvprop: 'content',
+                    rvslots: '*'
+                })
+            ).query.pages[0].revisions[0].slots.main.content
+        );
 
         toCheck.categories.forEach(async (check) => {
             const data = await new mw.Api()

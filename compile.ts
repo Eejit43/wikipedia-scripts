6c47824f47c5808e5a9c2b059dbcf608e2fe000c
Eejit <76887639+Eejit43@users.noreply.github.com>
2023-08-05 16:12:29 -0400

    feat: add js/css sourcemaps

--- compile.ts
+++ compile.ts
@@ -2,6 +2,6 @@ import { build } from 'esbuild';
 import postcss from 'esbuild-postcss';
 import { glob } from 'glob';
 
-build({ entryPoints: await glob('scripts/*.ts'), outdir: 'dist/scripts' });
+build({ entryPoints: await glob('scripts/*.ts'), outdir: 'dist/scripts', sourcemap: 'inline' });
 
-build({ entryPoints: await glob('styles/*.css'), outdir: 'dist/styles', plugins: [postcss()] });
+build({ entryPoints: await glob('styles/*.css'), outdir: 'dist/styles', plugins: [postcss()], sourcemap: 'inline' });

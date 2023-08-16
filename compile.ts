6b56c2e8464debdcea22d5d594322361da027eaf
Eejit <76887639+Eejit43@users.noreply.github.com>
2023-08-16 11:14:24 -0400

    refactor: remove usage of glob package

--- compile.ts
+++ compile.ts
@@ -1,7 +1,5 @@
 import { build } from 'esbuild';
 import postcss from 'esbuild-postcss';
-import { glob } from 'glob';
 
-build({ entryPoints: await glob('scripts/*.ts'), outdir: 'dist/scripts', sourcemap: 'inline' });
-
-build({ entryPoints: await glob('styles/*.css'), outdir: 'dist/styles', plugins: [postcss()], sourcemap: 'inline' });
+build({ entryPoints: ['scripts/*.ts'], outdir: 'dist/scripts', sourcemap: 'inline' });
+build({ entryPoints: ['styles/*.css'], outdir: 'dist/styles', plugins: [postcss()], sourcemap: 'inline' });

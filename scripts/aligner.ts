b617fad66f2cc68497a4071f1ec52fe8c9cd7b98
Eejit <76887639+Eejit43@users.noreply.github.com>
2024-03-11 13:10:50 -0400

    feat(aligner): prevent cursor from jumping to the end of text on click, improve loading checks

--- scripts/aligner.ts
+++ scripts/aligner.ts
@@ -1,140 +1,125 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface Window {
-    wikEd?: {
-        useWikEd: boolean;
-        UpdateTextarea: () => void; // eslint-disable-line @typescript-eslint/naming-convention
-        UpdateFrame: () => void; // eslint-disable-line @typescript-eslint/naming-convention
-    };
-}
+let alignerIsLoaded = false;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/naming-convention
-interface JQuery<TElement extends Node = HTMLElement> extends Iterable<TElement> {
-    textSelection(methodName: 'setContents', value: string): void;
-}
+/**
+ * Loads a link element to the menu.
+ */
+function loadLink() {
+    if (alignerIsLoaded) return;
+    alignerIsLoaded = true;
 
-(() => {
-    if (mw.config.get('wgNamespaceNumber') < 0) return; // Don't run in virtual namespaces
-    if (!mw.config.get('wgIsProbablyEditable')) return; // Don't run if user can't edit page
+    const link = mw.util.addPortletLink(mw.config.get('skin') === 'minerva' ? 'p-navigation' : 'p-cactions', '#', 'Align template parameters', 'align-parameters')!;
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
 
-    const searches = ['infobox', 'speciesbox', 'taxobox', 'automatic taxobox', 'osm location map', 'motorsport season'];
+        const editBox = $('#wpTextbox1');
+        if (!editBox) return mw.notify('Edit box not found, are you in edit mode?', { type: 'error', autoHideSeconds: 'short' });
 
-    mw.loader.using(['mediawiki.util', 'mediawiki.notification', 'jquery.textSelection'], () => {
-        const link = mw.util.addPortletLink(mw.config.get('skin') === 'minerva' ? 'p-navigation' : 'p-cactions', '#', 'Align template parameters', 'align-parameters')!;
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
+        const text = editBox.textSelection('getContents');
+        if (!text) return mw.notify('Edit box value not found!', { type: 'error', autoHideSeconds: 'short' });
 
-            if (window.wikEd?.useWikEd) window.wikEd.UpdateTextarea();
+        let count = 0;
 
-            const editBox = $('#wpTextbox1');
+        /**
+         * Finishes processing an infobox and updates the edit box contents.
+         * @param template The template string to process.
+         */
+        function processInfobox(template: string) {
+            if (template === '') return mw.notify('Infobox not found!', { type: 'error', autoHideSeconds: 'short' });
 
-            if (!editBox) return mw.notification.notify('Edit box not found, are you in edit mode?', { type: 'error', autoHideSeconds: 'short' });
+            if (open !== 0) return mw.notify('Template was not properly closed!', { type: 'error', autoHideSeconds: 'short' });
 
-            const text = editBox.textSelection('getContents');
+            let maxLength = 0;
 
-            if (!text) return mw.notification.notify('Edit box value not found!', { type: 'error', autoHideSeconds: 'short' });
+            const origTemplate = String(template);
+            const lines = template.split('\n');
+            const newLines = [];
 
-            let count = 0;
+            for (const line of lines) {
+                const parametersInLine = splitIntoParameters(line.trim());
 
-            /**
-             * Finishes processing an infobox and updates the edit box contents.
-             * @param template The template string to process.
-             */
-            function processInfobox(template: string) {
-                if (template === '') return mw.notification.notify('Infobox not found!', { type: 'error', autoHideSeconds: 'short' });
-
-                if (open !== 0) return mw.notification.notify('Template was not properly closed!', { type: 'error', autoHideSeconds: 'short' });
-
-                let maxLength = 0;
-
-                const origTemplate = String(template);
-                const lines = template.split('\n');
-                const newLines = [];
-
-                for (const line of lines) {
-                    const parametersInLine = splitIntoParameters(line.trim());
-
-                    for (const parameter of parametersInLine) {
-                        const line = parameter.trim();
-                        if (!line.startsWith('|') || line.split('=').length !== 2) {
-                            newLines.push(line);
-                            continue;
-                        }
-
-                        let [firstPart, lastPart] = splitParameter(line) as [string, string]; // eslint-disable-line prefer-const
-                        firstPart = firstPart.slice(1).trim();
-
-                        if (firstPart.length > maxLength) maxLength = firstPart.length;
-
-                        newLines.push('| ' + firstPart + '=' + lastPart);
-                    }
-                }
-
-                let output = '';
-
-                maxLength += 2; // to include '| '
-
-                for (let line of newLines) {
-                    const parts = splitParameter(line) as [string, string];
-
-                    if (parts.length < 2) {
-                        output += line += '\n';
+                for (const parameter of parametersInLine) {
+                    const line = parameter.trim();
+                    if (!line.startsWith('|') || line.split('=').length !== 2) {
+                        newLines.push(line);
                         continue;
                     }
 
-                    let firstPart = parts[0].trim();
+                    let [firstPart, lastPart] = splitParameter(line) as [string, string]; // eslint-disable-line prefer-const
+                    firstPart = firstPart.slice(1).trim();
 
-                    while (firstPart.length < maxLength) firstPart += ' ';
+                    if (firstPart.length > maxLength) maxLength = firstPart.length;
 
-                    output += firstPart + ' = ' + parts[1].trim() + '\n';
+                    newLines.push('| ' + firstPart + '=' + lastPart);
                 }
-
-                if (output.endsWith('\n')) output = output.slice(0, -1);
-
-                editBox.textSelection('setContents', editBox.textSelection('getContents').replace(origTemplate, output).replace(/\n+$/, ''));
-
-                if (window.wikEd?.useWikEd) window.wikEd.UpdateFrame();
             }
 
-            let template = '';
-            let open = 0;
+            let output = '';
 
-            for (let index = 0; index < text.length; index++) {
-                let foo = false;
+            maxLength += 2; // to include '| '
 
-                for (let search of searches) {
-                    search = '{{' + search;
-                    const searchLength = search.length;
+            for (let line of newLines) {
+                const parts = splitParameter(line) as [string, string];
 
-                    if (
-                        text.length - index > searchLength &&
-                        (text.slice(index, index + searchLength).toLowerCase() === search || text.slice(index, index + searchLength).toLowerCase() === search.replace(' ', '_'))
-                    ) {
-                        open++;
-                        template += text[index];
-                        foo = true;
-                    }
+                if (parts.length < 2) {
+                    output += line += '\n';
+                    continue;
                 }
 
-                if (open >= 1 && !foo) {
+                let firstPart = parts[0].trim();
+
+                while (firstPart.length < maxLength) firstPart += ' ';
+
+                output += firstPart + ' = ' + parts[1].trim() + '\n';
+            }
+
+            if (output.endsWith('\n')) output = output.slice(0, -1);
+
+            editBox.textSelection('setContents', editBox.textSelection('getContents').replace(origTemplate, output).replace(/\n+$/, ''));
+
+            editBox.textSelection('setSelection', { start: 0 });
+        }
+
+        let template = '';
+        let open = 0;
+
+        const searches = ['infobox', 'speciesbox', 'taxobox', 'automatic taxobox', 'osm location map', 'motorsport season'];
+
+        for (let index = 0; index < text.length; index++) {
+            let foo = false;
+
+            for (let search of searches) {
+                search = '{{' + search;
+                const searchLength = search.length;
+
+                if (
+                    text.length - index > searchLength &&
+                    (text.slice(index, index + searchLength).toLowerCase() === search || text.slice(index, index + searchLength).toLowerCase() === search.replace(' ', '_'))
+                ) {
+                    open++;
                     template += text[index];
-
-                    if (text[index] === '{') open++;
-                    else if (text[index] === '}') {
-                        open--;
-
-                        if (open === 0) {
-                            count++;
-                            processInfobox(template);
-                            template = '';
-                        }
-                    }
+                    foo = true;
                 }
             }
 
-            mw.notification.notify(`Successfully aligned ${count} templates!`, { type: 'success', autoHideSeconds: 'short' });
-        });
+            if (open >= 1 && !foo) {
+                template += text[index];
+
+                if (text[index] === '{') open++;
+                else if (text[index] === '}') {
+                    open--;
+
+                    if (open === 0) {
+                        count++;
+                        processInfobox(template);
+                        template = '';
+                    }
+                }
+            }
+        }
+
+        mw.notify(`Successfully aligned ${count} templates!`, { type: 'success', autoHideSeconds: 'short' });
     });
-})();
+}
 
 /**
  * Splits a string into an Array containing the key and value.
@@ -179,3 +164,13 @@ function splitIntoParameters(string: string): string[] {
 
     return parameters;
 }
+
+(() => {
+    if (mw.config.get('wgNamespaceNumber') < 0) return; // Don't run in virtual namespaces
+    if (!mw.config.get('wgIsProbablyEditable')) return; // Don't run if user can't edit page
+
+    mw.loader.using(['mediawiki.util', 'jquery.textSelection'], () => {
+        if (mw.config.get('wgAction') === 'edit') loadLink();
+        else mw.hook('wikipage.content').add(loadLink);
+    });
+})();

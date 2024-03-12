afd4b683d6d18eb588e027db412aae78180b9a9a
Eejit <76887639+Eejit43@users.noreply.github.com>
2024-03-11 22:59:50 -0400

    fix(aligner): load always, rather than trying to use buggy checks

--- scripts/aligner.ts
+++ scripts/aligner.ts
@@ -2,134 +2,119 @@
     if (mw.config.get('wgNamespaceNumber') < 0) return; // Don't run in virtual namespaces
     if (!mw.config.get('wgIsProbablyEditable')) return; // Don't run if user can't edit page
 
-    let alignerIsLoaded = false;
-
     mw.loader.using(['mediawiki.util', 'jquery.textSelection'], () => {
-        mw.hook('wikipage.content').add(loadLink);
+        const link = mw.util.addPortletLink(mw.config.get('skin') === 'minerva' ? 'p-navigation' : 'p-cactions', '#', 'Align template parameters', 'align-parameters')!;
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
 
-        /**
-         * Loads a link element to the menu.
-         */
-        // eslint-disable-next-line unicorn/consistent-function-scoping
-        function loadLink() {
-            if (alignerIsLoaded) return;
+            const editBox = $('#wpTextbox1');
+            if (editBox.length === 0) return mw.notify('Edit box not found!', { type: 'error', autoHideSeconds: 'short' });
 
-            if ($('#wpTextbox1').length === 0) return;
+            const text = editBox.textSelection('getContents');
+            if (!text) return mw.notify('Edit box value not found!', { type: 'error', autoHideSeconds: 'short' });
 
-            alignerIsLoaded = true;
+            let count = 0;
 
-            const link = mw.util.addPortletLink(mw.config.get('skin') === 'minerva' ? 'p-navigation' : 'p-cactions', '#', 'Align template parameters', 'align-parameters')!;
-            link.addEventListener('click', (event) => {
-                event.preventDefault();
+            /**
+             * Finishes processing an infobox and updates the edit box contents.
+             * @param template The template string to process.
+             */
+            function processInfobox(template: string) {
+                if (template === '') return mw.notify('Infobox not found!', { type: 'error', autoHideSeconds: 'short' });
 
-                const editBox = $('#wpTextbox1');
+                if (open !== 0) return mw.notify('Template was not properly closed!', { type: 'error', autoHideSeconds: 'short' });
 
-                const text = editBox.textSelection('getContents');
-                if (!text) return mw.notify('Edit box value not found!', { type: 'error', autoHideSeconds: 'short' });
+                let maxLength = 0;
 
-                let count = 0;
+                const origTemplate = String(template);
+                const lines = template.split('\n');
+                const newLines = [];
 
-                /**
-                 * Finishes processing an infobox and updates the edit box contents.
-                 * @param template The template string to process.
-                 */
-                function processInfobox(template: string) {
-                    if (template === '') return mw.notify('Infobox not found!', { type: 'error', autoHideSeconds: 'short' });
+                for (const line of lines) {
+                    const parametersInLine = splitIntoParameters(line.trim());
 
-                    if (open !== 0) return mw.notify('Template was not properly closed!', { type: 'error', autoHideSeconds: 'short' });
-
-                    let maxLength = 0;
-
-                    const origTemplate = String(template);
-                    const lines = template.split('\n');
-                    const newLines = [];
-
-                    for (const line of lines) {
-                        const parametersInLine = splitIntoParameters(line.trim());
-
-                        for (const parameter of parametersInLine) {
-                            const line = parameter.trim();
-                            if (!line.startsWith('|') || line.split('=').length !== 2) {
-                                newLines.push(line);
-                                continue;
-                            }
-
-                            let [firstPart, lastPart] = splitParameter(line) as [string, string]; // eslint-disable-line prefer-const
-                            firstPart = firstPart.slice(1).trim();
-
-                            if (firstPart.length > maxLength) maxLength = firstPart.length;
-
-                            newLines.push('| ' + firstPart + '=' + lastPart);
-                        }
-                    }
-
-                    let output = '';
-
-                    maxLength += 2; // to include '| '
-
-                    for (let line of newLines) {
-                        const parts = splitParameter(line) as [string, string];
-
-                        if (parts.length < 2) {
-                            output += line += '\n';
+                    for (const parameter of parametersInLine) {
+                        const line = parameter.trim();
+                        if (!line.startsWith('|') || line.split('=').length !== 2) {
+                            newLines.push(line);
                             continue;
                         }
 
-                        let firstPart = parts[0].trim();
+                        let [firstPart, lastPart] = splitParameter(line) as [string, string]; // eslint-disable-line prefer-const
+                        firstPart = firstPart.slice(1).trim();
 
-                        while (firstPart.length < maxLength) firstPart += ' ';
+                        if (firstPart.length > maxLength) maxLength = firstPart.length;
 
-                        output += firstPart + ' = ' + parts[1].trim() + '\n';
+                        newLines.push('| ' + firstPart + '=' + lastPart);
                     }
-
-                    if (output.endsWith('\n')) output = output.slice(0, -1);
-
-                    editBox.textSelection('setContents', editBox.textSelection('getContents').replace(origTemplate, output).replace(/\n+$/, ''));
-
-                    editBox.textSelection('setSelection', { start: 0 });
                 }
 
-                let template = '';
-                let open = 0;
+                let output = '';
 
-                const searches = ['infobox', 'speciesbox', 'taxobox', 'automatic taxobox', 'osm location map', 'motorsport season'];
+                maxLength += 2; // to include '| '
 
-                for (let index = 0; index < text.length; index++) {
-                    let foo = false;
+                for (let line of newLines) {
+                    const parts = splitParameter(line) as [string, string];
 
-                    for (let search of searches) {
-                        search = '{{' + search;
-                        const searchLength = search.length;
-
-                        if (
-                            text.length - index > searchLength &&
-                            (text.slice(index, index + searchLength).toLowerCase() === search || text.slice(index, index + searchLength).toLowerCase() === search.replace(' ', '_'))
-                        ) {
-                            open++;
-                            template += text[index];
-                            foo = true;
-                        }
+                    if (parts.length < 2) {
+                        output += line += '\n';
+                        continue;
                     }
 
-                    if (open >= 1 && !foo) {
+                    let firstPart = parts[0].trim();
+
+                    while (firstPart.length < maxLength) firstPart += ' ';
+
+                    output += firstPart + ' = ' + parts[1].trim() + '\n';
+                }
+
+                if (output.endsWith('\n')) output = output.slice(0, -1);
+
+                editBox.textSelection('setContents', editBox.textSelection('getContents').replace(origTemplate, output).replace(/\n+$/, ''));
+
+                editBox.textSelection('setSelection', { start: 0 });
+            }
+
+            let template = '';
+            let open = 0;
+
+            const searches = ['infobox', 'speciesbox', 'taxobox', 'automatic taxobox', 'osm location map', 'motorsport season'];
+
+            for (let index = 0; index < text.length; index++) {
+                let foo = false;
+
+                for (let search of searches) {
+                    search = '{{' + search;
+                    const searchLength = search.length;
+
+                    if (
+                        text.length - index > searchLength &&
+                        (text.slice(index, index + searchLength).toLowerCase() === search || text.slice(index, index + searchLength).toLowerCase() === search.replace(' ', '_'))
+                    ) {
+                        open++;
                         template += text[index];
-
-                        if (text[index] === '{') open++;
-                        else if (text[index] === '}') {
-                            open--;
-
-                            if (open === 0) {
-                                count++;
-                                processInfobox(template);
-                                template = '';
-                            }
-                        }
+                        foo = true;
                     }
                 }
 
-                mw.notify(`Successfully aligned ${count} templates!`, { type: 'success', autoHideSeconds: 'short' });
-            });
-        }
+                if (open >= 1 && !foo) {
+                    template += text[index];
+
+                    if (text[index] === '{') open++;
+                    else if (text[index] === '}') {
+                        open--;
+
+                        if (open === 0) {
+                            count++;
+                            processInfobox(template);
+                            template = '';
+                        }
+                    }
+                }
+            }
+
+            mw.notify(`Successfully aligned ${count} templates!`, { type: 'success', autoHideSeconds: 'short' });
+        });
     });
 })();
 

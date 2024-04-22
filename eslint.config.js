fc033a91c046e89296275a513c7240010946a146
Eejit <76887639+Eejit43@users.noreply.github.com>
2024-04-21 22:15:16 -0400

    chore: rm globals from eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 // @ts-check
 
 import sharedConfig from '@eejit/eslint-config-typescript';
-import globals from 'globals';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
@@ -9,7 +8,6 @@ export default [
     {
         languageOptions: {
             parserOptions: { project: ['./tsconfig.json', './scripts/tsconfig.json'] },
-            globals: { ...globals.browser },
         },
         rules: {
             'unicorn/catch-error-name': ['error', { ignore: ['errorCode'] }],

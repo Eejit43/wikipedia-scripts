f2ed0d198e56f89e13b5326324430614ba0929f8
Eejit <76887639+Eejit43@users.noreply.github.com>
2025-04-29 19:14:42 -0400

    fix(article-cleaner): fix regex timeout

--- scripts/article-cleaner.ts
+++ scripts/article-cleaner.ts
@@ -479,9 +479,10 @@ function cleanupSpacing(content: string, secondRun = false) {
  * @param content The article content to clean up.
  */
 function cleanupReferences(content: string) {
-    content = content
-        .replaceAll(/((?:<ref(?!erences)[^/]*?>.*?<\/ref>)+)([!,.;?])/g, '$2$1')
-        .replaceAll(/((?:<ref(?!erences)(?:.(?!<\/ref))*?\/>)+)([!,.;?])/g, '$2$1'); // Fix punctuation following references
+    content = content.replaceAll(
+        /((?:<ref(?!erences)[^/]*?>(?:.(?!<\/ref))*?<\/ref>|<ref(?!erences)(?:.(?!<\/ref))*?\/>)+)([!,.;?])/g,
+        '$2$1',
+    ); // Fix punctuation following references
 
     const references: { start: number; end: number; isSelfClosing?: true }[] = [];
 

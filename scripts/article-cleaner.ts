08d6493281f0017e4c3030cecc3c0f8a8f36ae5b
Eejit <76887639+Eejit43@users.noreply.github.com>
2025-07-07 22:59:37 -0400

    feat(article-cleaner): maintain spacing inside references tags

--- scripts/article-cleaner.ts
+++ scripts/article-cleaner.ts
@@ -561,7 +561,7 @@ function cleanupStrayMarkup(content: string) {
 function cleanupSpacing(content: string, secondRun = false) {
     const PLACEHOLDER = '\u{F0000}';
 
-    const TAGS_TO_IGNORE = ['poem', 'pre', 'templatedata'];
+    const TAGS_TO_IGNORE = ['poem', 'pre', 'references', 'templatedata'];
 
     const ignoredTagsContent: string[] = [];
 

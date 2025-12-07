a4deaed4d1ca8d3e18324c48214debcffaf5cfec
Eejit <76887639+Eejit43@users.noreply.github.com>
2025-12-07 13:48:50 -0500

    chore(eslint.config.js): ignore dist and deprecated scripts

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
 // @ts-check
 
 import sharedConfig from '@eejit/eslint-config-typescript';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-export default defineConfig(sharedConfig, {
-    languageOptions: { parserOptions: { project: ['./tsconfig.json', './scripts/tsconfig.json'] } },
-    rules: { 'unicorn/catch-error-name': ['error', { ignore: ['errorCode'] }] },
-});
+export default defineConfig(
+    sharedConfig,
+    {
+        languageOptions: { parserOptions: { project: ['./tsconfig.json', './scripts/tsconfig.json'] } },
+        rules: { 'unicorn/catch-error-name': ['error', { ignore: ['errorCode'] }] },
+    },
+    globalIgnores(['dist/*', 'scripts/AFCRHS.ts', 'scripts/pageswap.ts']),
+);
